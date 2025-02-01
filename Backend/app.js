9c4d8a82750c92cd0aff69dd8571381f35819d6f
dhruv jain <dhruvdkjk@gmail.com>
2025-02-02 00:17:40 +0530

    Added frontend inside backend

--- Backend/app.js
+++ Backend/app.js
@@ -5,15 +5,29 @@ import cors from 'cors';
 
 import BookRoute from './routes/book.route.js';
 import UserRoute from './routes/user.route.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MONGODBURI;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 app.use(cors());
 app.use(express.json());
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'Frontend/dist')));
+
+// Handles any requests that don't match the ones above
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/Frontend/dist/index.html'));
+}
+);
 
 try {
   mongoose.connect(URI, {});
