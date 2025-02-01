4403a268a4d976b3483f7f37cbf7131ef2509ac7
dhruv jain <dhruvdkjk@gmail.com>
2025-02-01 22:35:55 +0530

    updated tsconfig frontend

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode:"class",
+  darkMode: "class",
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [daisyui],
   daisyui: {
     themes: ["light"], // Disable dark theme and keep only the light theme
   },
-}
+};
